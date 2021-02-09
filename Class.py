11d1ffba88e109ecffcad8adef956e6556f4a552
ChunChunNi <1808790564@qq.com>
2021-02-09 17:17:00 +0800

    第十次提交

--- Class.py
+++ Class.py
@@ -1,4 +1,4 @@
-'''
+'''                                     #类
 class Dog():
     """
     定义一个Dog类
@@ -31,18 +31,24 @@ class Cars():
         self.year = year
         self.odometer_reading = 0       #设置默认属性值
 
-    def get_descriptve(self):           #描述车辆
+    def get_describe(self):           #描述车辆
         long_name = str(self.year) + ' ' + self.make + ' ' + self.model
         return long_name.title()
 
     def read_odometer(self):            #打印里程
         print("This car has " + str(self.odometer_reading) + " miles on it.")
     
-    def update_odometers(self, mile):
-        self.odometer_reading = mile
+    def update_odometers(self, miles):  #修改属性的方法
+        self.odometer_reading = miles
+
+    def increment_odometer(self, miles):#递增属性值的方法
+        self.odometer_reading += miles
+
+    def fill_gas(self):                     #加油
+        print("Gas Tank is fill.")
 
 my_car = Cars("audi", "A4", 2016)
-print(my_car.get_descriptve())
+print(my_car.get_describe())
 my_car.read_odometer()                  #打印默认属性值
 
 my_car.odometer_reading = 2020          #直接修改属性值
@@ -50,3 +56,46 @@ my_car.read_odometer()
 
 my_car.update_odometers(2009)           #通过方法修改属性值
 my_car.read_odometer()
+
+my_car.increment_odometer(100)          #通过方法增加属性值
+my_car.read_odometer()
+
+"""
+继承：子类可以继承父类所有的属性和方法，同时可以定义自己的属性和方法
+
+class ElectricCars(Cars):               #继承自Cars的ElectrinCars类
+    def __init__(self, make, model, year):
+    #初始化父类属性
+        super().__init__(make, model, year)
+    #super是一个特殊函数，用于将子类和父类关联起来。让Python调用Cars的方法__init__()，让ElectrinCars实例包含父类的所有属性
+        self.battery_size = 70          #新建子类的属性
+    
+    def describe_battery(self):         #新建子类的方法
+        print("This car has a " + str(self.battery_size) + "-kwh battery.")
+
+    def fill_gas(self):                 #重写继承自父类的方法
+        print("This car has no gas_tank.")
+
+my_ElCars = ElectricCars('tesla', "Model's", 2016)
+print(my_ElCars.get_describe())         #子类中直接继承了所有父类的方法
+my_ElCars.describe_battery()            #调用子类新创建的方法
+
+my_car.fill_gas()                       #加油方法
+my_ElCars.fill_gas()                    #重写加油方法
+"""
+
+class Battery():                        #新建电池类
+    def __init__(self, battery_size = 70):
+        self.battery_size = battery_size
+    
+    def describe_battery(self):
+        print("This car has a " + str(self.battery_size) + "-kwh battery.")
+
+class ElectricCars(Cars):               #继承自Cars的ElectrinCars类
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery = Battery()        #将实例对象用作属性
+    
+my_ElCars = ElectricCars('tesla', "Model's", 2016)
+print(my_ElCars.get_describe())
+my_ElCars.battery.describe_battery()    #在实例my_ElCars中找到属性battery，再在该属性中找到battery实例可以调用的方法
