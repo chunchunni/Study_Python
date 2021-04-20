0ec6f97aee63086ee4e040f341223dd025458c29
chunchunni <1808790564@qq.com>
2021-04-20 00:01:12 -0700

    0

--- Alien_Attack/alien_invasion.py
+++ Alien_Attack/alien_invasion.py
@@ -1,20 +1,24 @@
 import sys
 import pygame
+from settings import Settings
 
 def run_game():
     # 初始化游戏
     pygame.init()
-    # 实参元组（1200，800）用来指定游戏窗口的大小
-    screen = pygame.display.set_mode((1200,800))
+    ai_settings = Settings()
+    # 实参元组（*，*）用来指定游戏窗口的大小
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_heigt))
     # 创建窗口
-    pygame.display.set_caption("Alien Invasion")
+    pygame.display.set_caption("Alien Invasion")  
 
     while True:
         for event in pygame.event.get():
             # 单击关闭窗口
             if event.type == pygame.QUIT:
                 sys.exit()
-    # 更新屏幕，隐藏旧屏幕
-    pygame.display.flip()
+        # 更新时会重新绘制屏幕
+        screen.fill(ai_settings.bg_color)
+        # 更新屏幕，隐藏旧屏幕
+        pygame.display.flip()
 
 run_game()
---
+++ Alien_Attack/settings.py
@@ -0,0 +1,8 @@
+class Settings():
+    # 用于存放所有关于游戏的设置
+    def __init__(self):
+        # 屏幕设置
+        self.screen_width = 1200
+        self.screen_heigt = 800
+        # 设置背景色
+        self.bg_color = (230,230,230)
